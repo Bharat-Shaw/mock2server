12c42728434c7ac03dcf0deb195f57a072e3e303
Bharat Shaw <122665913+Bharat-Shaw@users.noreply.github.com>
2023-06-30 23:23:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-const PORT = process.env.PORT || 3000;
-const path = require("path");
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db.json"));
-const middlewares = jsonServer.defaults();
-const cors=require('cors')
 const express = require('express');
-server.use(express.json());
-server.use(cors());
-server.use(middlewares);
-server.use(jsonServer.bodyParser);
-server.use("/api", router);
-server.listen(PORT);
+const jsonServer = require('json-server');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use('/api', middlewares);
+app.use('/api', router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
